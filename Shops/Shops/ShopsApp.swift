f7d7c4df0e90483101e4a494a058644957d2d5db
Macostik <y.granchenko@gmail.com>
2021-01-18 11:00:27 +0200

    create app

---
+++ Shops/Shops/ShopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopsApp.swift
+//  Shops
+//
+//  Created by Maxim Granchenko on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
